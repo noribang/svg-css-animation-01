763c7c13f234892d62379e47b34b1fb77aaf030d
hoonkim <hoonk9387@gmail.com>
2023-04-29 18:22:46 -0500

    Add addeventlistener to buttons.

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ let cat = document.getElementsByClassName("cat")[0];
 // console.log(btns);
 // console.log(cat);
 
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", manageCatClasses);
+}
+
+function manageCatClasses() {
+    alert("testing 123!!!");
+}
